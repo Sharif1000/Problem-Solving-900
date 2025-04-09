b7c19826aea87c23ab421928cf081a01a21dd2b7
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-09 09:13:10 +0600

    Create Problem_005.cpp

---
+++ Problem_005.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+..  int n,k; cin>>n>>k;
+..  if(k == 1){
+...   
+...   }
+    }
+    return 0;
+}
