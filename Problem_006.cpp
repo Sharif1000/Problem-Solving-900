c7f75dd61df281cc8196c57d4b117b4884a668ed
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-12 18:01:20 +0600

    Create Problem_006.cpp

---
+++ Problem_006.cpp
@@ -0,0 +1,10 @@
+--Problem Link: https://codeforces.com/contest/84/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n; cin>>n;
+    cout<<(3 * n)/2;
+    return 0;
+}
