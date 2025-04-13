a60e28825c84c89addd555365ae0392e9755755d
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-13 08:22:45 +0600

    Update Problem_007.cpp

--- Problem_007.cpp
+++ Problem_007.cpp
@@ -1,15 +1,24 @@
+--Problem Link: https://codeforces.com/contest/267/problem/A
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     int t; cin>>t;
-..  while(t--){
-...   int a, b; cin>>a>>b;
-..... int cnt = 0;
-....  while(a > 0 && b > 0){
-......  if(
-}
-
-}
+    while(t--){
+        int a, b; cin>>a>>b;
+        int cnt = 0;
+        while(a > 0 && b > 0){
+            if(a > b){
+                cnt += a / b;
+                a = a % b;
+            }
+            else{
+                cnt += b / a;
+                b = b % a;
+            }
+        }
+        cout<<cnt<<endl;
+    }
     return 0;
 }
